8fa7e287f144e2fa467cb1e14739a4691175b243
Michael Cardoso <michaelcardoso97@gmail.com>
2025-06-19 13:31:02 -0300

    Exibição e teste de tabuleiro

--- src/main/java/com/br/michael/cm/Aplicacao.java
+++ src/main/java/com/br/michael/cm/Aplicacao.java
@@ -1,4 +1,17 @@
 package com.br.michael.cm;
 
+import com.br.michael.cm.modelo.Tabuleiro;
+
 public class Aplicacao {
+
+    public static void main(String[] args) {
+
+        Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+        tabuleiro.abrir(3, 3);
+        tabuleiro.abrir(1, 3);
+        tabuleiro.alterarMarcacao(4, 4);
+        tabuleiro.alterarMarcacao(2, 4);
+
+        System.out.println(tabuleiro.toString());
+    }
 }
