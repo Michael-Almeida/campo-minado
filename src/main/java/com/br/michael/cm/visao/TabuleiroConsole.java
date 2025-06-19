80c22d52542d8e505fa656c7ad45c0bafe17ed59
Michael Cardoso <michaelcardoso97@gmail.com>
2025-06-19 16:29:14 -0300

    Ajuste na lógica de opções abrir ou marcar

--- src/main/java/com/br/michael/cm/visao/TabuleiroConsole.java
+++ src/main/java/com/br/michael/cm/visao/TabuleiroConsole.java
@@ -46,11 +46,10 @@ public class TabuleiroConsole {
                 String digitado = capturarValorDigitado("Digite (x, y): ");
                 Iterator<Integer> xy = Arrays.stream(digitado.split(",")).map(e -> Integer.parseInt(e.trim())).iterator();
 
-                System.out.println(xy.next());
-                System.out.println(xy.next());
 
-                digitado = capturarValorDigitado("1 - Abrir ou 2 - (Des)marcar");
+                digitado = capturarValorDigitado("1 - Abrir ou 2 - (Des)marcar: ");
 
+                System.out.println(digitado);
                 if ("1".equals(digitado)) {
                     tabuleiro.abrir(xy.next(), xy.next());
                 } else if ("2".equals(digitado)) {
