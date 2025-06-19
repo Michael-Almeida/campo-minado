43620b653e81d46e2639cfd9ced1e77c658c85c0
Michael Cardoso <michaelcardoso97@gmail.com>
2025-06-19 16:13:01 -0300

    Interação através do terminal

--- src/main/java/com/br/michael/cm/Aplicacao.java
+++ src/main/java/com/br/michael/cm/Aplicacao.java
@@ -1,17 +1,14 @@
 package com.br.michael.cm;
 
 import com.br.michael.cm.modelo.Tabuleiro;
+import com.br.michael.cm.visao.TabuleiroConsole;
 
 public class Aplicacao {
 
     public static void main(String[] args) {
 
         Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
-        tabuleiro.abrir(3, 3);
-        tabuleiro.abrir(1, 3);
-        tabuleiro.alterarMarcacao(4, 4);
-        tabuleiro.alterarMarcacao(2, 4);
 
-        System.out.println(tabuleiro.toString());
+        new TabuleiroConsole(tabuleiro);
     }
 }
